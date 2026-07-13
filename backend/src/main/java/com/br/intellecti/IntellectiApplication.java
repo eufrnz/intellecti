0787ed67cff70b25fdb7e134a804a6ea7ea60741
danfornazeiro <danielacfornazeiro123@usp.br>
2026-07-13 17:55:52 -0300

    first commit

---
+++ backend/src/main/java/com/br/intellecti/IntellectiApplication.java
@@ -0,0 +1,13 @@
+package com.br.intellecti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntellectiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntellectiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/br/intellecti/IntellectiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.intellecti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntellectiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
